7df62bae98a1ac80d73294b083d16070222a2720
kajirikajiri <kajirikajiri@gmail.com>
2021-01-29 08:56:56 +0900

    :sparkles: (add prettier lint)

--- src/popup/EventPlayer/Main/EventsComponent/index.tsx
+++ src/popup/EventPlayer/Main/EventsComponent/index.tsx
@@ -3,28 +3,25 @@ import { execEvent } from "../../../../scripts/execEvent";
 import { Event } from "../types/event";
 
 interface Props {
-  events: Event[]
+  events: Event[];
 }
 
-export const EventsComponent:FC<Props>=({events})=>{
-
-  const handleClick=(selector, index)=>{
-    execEvent(selector, index)
-  }
+export const EventsComponent: FC<Props> = ({ events }) => {
+  const handleClick = (selector, index) => {
+    execEvent(selector, index);
+  };
 
   return (
     <>
-    {
-      events.map(({selector, index})=>{
-      return (
-        <div>
-          <button onClick={()=>handleClick(selector, index)}>
-            {index}
-          </button>
-        </div>
-      )
-      })
-    }
-  </>
-  )
-}
+      {events.map(({ selector, index }) => {
+        return (
+          <div>
+            <button onClick={() => handleClick(selector, index)}>
+              {index}
+            </button>
+          </div>
+        );
+      })}
+    </>
+  );
+};
